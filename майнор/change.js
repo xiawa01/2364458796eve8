b43b23eed53c197475bace950bba4a8916bd3d09
xiawa01 <eva.zyryanova@gmail.com>
2024-09-19 20:46:39 +0300

    Update change.js

--- майнор/change.js
+++ майнор/change.js
@@ -1,1 +1,20 @@
+const change_= document.getElementById("change_")
 
+function change_all(){
+
+    if(document.getElementsByTagName('div')[3].innerText==='Zyryanova'){
+        document.getElementsByTagName('div')[3].innerText='Зырянова';
+        document.getElementsByTagName('div')[4].innerText='Ева';
+        document.getElementsByTagName('div')[5].innerText='Игоревна';
+
+
+    }
+    else{
+        if (document.getElementsByTagName('div')[3].innerText==='Зырянова'){
+            document.getElementsByTagName('div')[3].innerText='Zyryanova';
+            document.getElementsByTagName('div')[4].innerText='Eva';
+            document.getElementsByTagName('div')[5].innerText='Igorevna';
+        }
+    }
+}
+change_.addEventListener("click",change_all,)
